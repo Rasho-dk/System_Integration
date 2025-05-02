9d65a6645212690ce917e00c207309a9d6545d63
Shero Shvan <sheroshvan@outlook.dk>
2025-05-02 13:48:24 +0200

    25._GitHub_Webhooks-webhook system

--- 25._GitHub_Webhooks/02._python/main.py
+++ 25._GitHub_Webhooks/02._python/main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI, Request, Response
 import json 
+import httpx
 
 app = FastAPI()
 
@@ -20,3 +21,32 @@ async def github_webhook(request: Request, response: Response):
         response.status_code = 204
     else: 
         response.status_code = 400
+
+
+@app.post("/mywebhook")
+async def webhook(request: Request):
+    data = await request.body()
+    print(json.loads(data))
+    return
+
+
+
+@app.post("/register")
+async def register():
+    url = "https://webhoob20250501214221-cad2cdb4hncsg6bu.northeurope-01.azurewebsites.net/api/Webhooks"
+    payload = {
+        "callbackUrl": "https://rasho.loca.lt/mywebhook",
+        "eventTypes": [
+            "PaymentInitiated",
+            "PaymentFailed"
+        ]
+    }        
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, json=payload)
+        print(response.status_code)
+        print(response.text)
+    
+ 
